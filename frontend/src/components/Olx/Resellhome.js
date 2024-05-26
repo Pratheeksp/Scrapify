247450961f45fce560efeb705a392c593a961584
RohanAcharV <rohanacharv@gmail.com>
2024-05-26 22:50:24 +0530

    Pincode changes

--- frontend/src/components/Olx/Resellhome.js
+++ frontend/src/components/Olx/Resellhome.js
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button, Card, CardActionArea, CardContent, CardMedia, Grid, IconButton, InputAdornment, TextField, Toolbar, Typography ,LinearProgress, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, ButtonGroup, Stack, Divider,Paper} from "@mui/material";
+import { AppBar, Box, Button, Card, CardActionArea, CardContent, CardMedia, Grid, IconButton, InputAdornment, TextField, Toolbar, Typography ,LinearProgress, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, ButtonGroup, Stack, Divider,Paper, Pagination} from "@mui/material";
 import React, { useEffect, useRef, useState } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -18,124 +18,55 @@ import { styled } from '@mui/material/styles';
 function Resell(){
   const [items,setitems]=useState();
   const [filterkey,setfilterkey]=useState('');
-  // const [searchkey,setsearchkey]=useState('');
-  const pagelimit=10;
+  const [searchkey,setsearchkey]=useState('');
+  const pagelimit=8;
   const [pageno,setpageno]=useState(1);
   const navigate =useNavigate();
   
-  const fetchInitial = async () => {
+  const fetchData = async (direction) => {
     let q;
-    if(filterkey===''){
-       q = query(
-        collection(db, 'resellDoc'),
-        orderBy('timestamp', 'desc'),
-        limit(pagelimit)
-      );
-    }
-    else{
-      q = query(
-        collection(db, 'resellDoc'),
-        or(where('vehicleType','==',filterkey),where('electronicsType','==',filterkey),
+    q = query(
+      collection(db, 'resellDoc'),
+      orderBy('timestamp', 'desc'),
+    );
+
+    if(filterkey!==''){
+      q=query(q, or(where('vehicleType','==',filterkey),where('electronicsType','==',filterkey),
         where('mobileType','==',filterkey),
         where('propertyType','==',filterkey)
-        ),
-        orderBy('timestamp', 'desc'),
-        limit(pagelimit)
-      );
+        ))
     }
-  
-    const querySnapshot = await getDocs(q);
-    const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    
-    console.log(data);
-    setitems(data);
-  };
-  
-
-  const nextPage = async () => {
+   if(direction==""){}
+   else if(direction=="prev"){
+      if(pageno===1) return;
+      const firstData = items[0];
+      q=query(q,endBefore(firstData['timestamp']))
+      setpageno((prevpageno)=>prevpageno-1)
+   }
+   else{
     if(items.length<pagelimit) return;
-    
-    const lastData = items[items.length - 1];
+      const lastData = items[items.length - 1];
+      q=query(q,startAfter(lastData['timestamp']))
+      setpageno((prevpageno)=>prevpageno+1)
+   }
   
-    let q;
-    if(filterkey===''){
-      q= query(collection(db, 'resellDoc'),
-      orderBy('timestamp', 'desc'),
-      startAfter(lastData['timestamp']),
-      limit(pagelimit)
-      );
-    }
-    else{
-      q = query(
-        collection(db, 'resellDoc'),
-        or(where('vehicleType','==',filterkey),where('electronicsType','==',filterkey),
-        where('mobileType','==',filterkey),
-        where('propertyType','==',filterkey)
-        ),
-        
-        orderBy('timestamp', 'desc'),
-        startAfter(lastData['timestamp']),
-        limit(pagelimit)
-      );
-    }
-
     const querySnapshot = await getDocs(q);
-    const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    let data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     if(data.length==0)return;
-    setpageno((prevpageno)=>prevpageno+1)
-    console.log(data);
-    setitems(data);
-    console.log('next',pageno)
-  };
-  
-  const prevPage = async () => {
-    if(pageno===1) return;
-    
-    const firstData = items[0];
-  
-    let q;
-    if(filterkey===''){
-      q= query(collection(db, 'resellDoc'),
-      orderBy('timestamp', 'desc'),
-      endBefore(firstData['timestamp']),
-      limit(pagelimit)
-      );
-    }
-    else{
-      q = query(
-        collection(db, 'resellDoc'),
-        or(where('vehicleType','==',filterkey),where('electronicsType','==',filterkey),
-        where('mobileType','==',filterkey),
-        where('propertyType','==',filterkey)
-        ),
-        
-        orderBy('timestamp', 'desc'),
-        endBefore(firstData['timestamp']),
-        limit(pagelimit)
-      );
-    }
 
-    const querySnapshot = await getDocs(q);
-    const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    setpageno((prevpageno)=>prevpageno-1)
-    console.log(data);
+    if (searchkey) {
+      const lowerSearchKey = searchkey.toLowerCase();
+      data = data.filter(item => (item.model && item.model.toLowerCase().includes(lowerSearchKey)) || (item.propertyType && item.propertyType.toLowerCase().includes(lowerSearchKey)) || (item.transactionType && item.transactionType.toLowerCase().includes(lowerSearchKey)));
+    }
+    data=data.slice(0,pagelimit)
     setitems(data);
-    console.log('next',pageno)
   };
 
-  useEffect(() => {
-    fetchInitial();
-  }, []);
-
-//  useEffect(() => {
-//     fetchInitial();
-//   }, [searchkey]);
-
-  useEffect(()=>{
-    fetchInitial();
-    console.log(filterkey)
+ useEffect(() => {
+    fetchData("");
     setCategory(null)
-  },[filterkey])
+  }, [searchkey,filterkey]);
+
 
   const [category, setCategory] = useState(null);
 
@@ -155,12 +86,9 @@ function Resell(){
 
     return (
 <Box >
-{/* <Box sx={{ flexGrow: 1 }}>
-      
-      <AppBar position="static" sx={{color:'black',backgroundColor:'white',border:'none',boxShadow:'none'}}>
-        <Toolbar sx={{display:'flex',justifyContent:'space-evenly'}}>
-            
-            <Box sx={{textAlign:"center"}}><TextField   variant="outlined" 
+  <Box sx={{  backgroundColor: 'rgba(173, 216, 230, 0.2)',}}>
+  <Box sx={{textAlign:"center"}}>
+    <TextField   variant="outlined" 
             InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
@@ -168,12 +96,7 @@ function Resell(){
                   </InputAdornment>
                 ),
               }}
-              placeholder=" Find items .." sx={{fontSize:{xs:'12px',sm:"14px",md:"16px"},width:'70vw',padding:"10px 0 20px "}} onChange={(e)=>setsearchkey(e.target.value)}/></Box>
-        </Toolbar>
-      </AppBar>
-    </Box> */}
-
-  <Box sx={{  backgroundColor: 'rgba(173, 216, 230, 0.2)', margin:0,p:0}}>
+              placeholder=" Find items .." sx={{fontSize:{xs:'12px',sm:"14px",md:"16px"},width:'70vw',margin:'20px 0',backgroundColor:'white'}} onChange={(e)=>setsearchkey(e.target.value)}/></Box>
     <Typography sx={{padding:'2vh 2vw',fontWeight:'bold',color:'grey',fontSize:{xs:'12px',sm:'16px',md:'20px'}}}>Browse by categories </Typography> 
      <div style={{ display: 'flex', justifyContent: 'space-evenly' ,width:'100vw'}}>
   <Button sx={{ fontSize: { xs: '12px', sm: '16px', md: '20px' }, color: 'black', textTransform: 'none', borderBottom: category === 'Vehicle' ? '3px solid black' : 'none'  }} onClick={() => handleCategoryClick('Vehicle')}>Vehicle</Button>
@@ -221,7 +144,7 @@ function Resell(){
 
 {
   (items!=='undefined' && items!=undefined)?(
-    <Box sx={{paddingBottom:'20px'}}><Box margin={'20px 0px'} sx={{width:'100vw'}}>
+    <Box sx={{paddingBottom:'0px'}}><Box margin={'20px 0px'} sx={{width:'100vw'}}>
       {
         (filterkey=='') && (<Box><Typography sx={{margin:'10px 15px',fontWeight:'bold',fontSize: { xs: '14px', sm: '16px', md: '20px' }}}>Fresh recomendations</Typography><br /></Box>)
       }
@@ -258,11 +181,13 @@ function Resell(){
 
 {
   (items.length!==0)?(
-    <Box sx={{display:'flex',justifyContent:'center',mt:'600px',position:'fixed',bottom:0,width:'97vw'}}>
-            <Button sx={{margin:'0 1vw'}} startIcon={<ArrowBackIos/>} variant="contained" onClick={prevPage}>Prev</Button>
-            <Button sx={{margin:'0 1vw'}} endIcon={<ArrowForwardIos/>} variant="contained" onClick={nextPage}>Next</Button>
-      </Box>
-    // <Typography>Hello</Typography>
+  <>
+ <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%' }}>
+  <IconButton sx={{ border: '2px solid black', backgroundColor: 'white', '&:hover': { backgroundColor: 'lightblue' } }} onClick={()=>fetchData("prev")}><ArrowBackIos /></IconButton>&nbsp;
+  <IconButton sx={{ fontWeight: 'bold', color: 'black',}}>{pageno}</IconButton>&nbsp;
+  <IconButton sx={{ border: '2px solid black', backgroundColor: 'white', '&:hover': { backgroundColor: 'lightblue' } }} onClick={()=>fetchData("next")}><ArrowForwardIos /></IconButton>
+</Box>
+      </>
       ):(
         <Box><Typography sx={{textAlign:'center',marginTop:'5vh'}}>Apologies, but we currently don't have any data available for this category.</Typography></Box>
       )
@@ -274,26 +199,6 @@ function Resell(){
   )
 }
   
-
-
-<Box sx={{ position: 'sticky', bottom: 5,  textAlign: 'center' }}>
-  {/* <Button
-  sx={{
-    border: '5px solid #3498db',
-    borderRadius: '15px',
-    color: '#ffffff',
-    backgroundColor: '#7fb3d5',
-    fontWeight: 'bolder',
-    width: { xs: '100px', sm: '140px', md: '160px' },
-    height: { xs: '50px', sm: '60px', md: '70px' },
-  }}
->
-        <Typography fontWeight="bold" fontSize="large">
-          &#43;
-        </Typography>
-        &nbsp; SELL
-      </Button> */}
-    </Box>
 
 </Box>
     )
